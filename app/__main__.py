896ffaa6e96d9caa4a73bd8d437ada09ed00e9a0
Luka Kavčič <luka.kavcic95@gmail.com>
2021-03-19 10:24:13 +0100

    crawl with workers

--- app/__main__.py
+++ app/__main__.py
@@ -1,13 +1,40 @@
 from crawler import Crawler
 from page_handler import PageHandler
+from config import Config
+
+import concurrent.futures
+import threading
+import time
+import copy
+import sys
+
+lock = threading.Lock()
 
 # config options
-SEED = False
-SEED_PATH = 'seed.txt'
+SEED = Config.SEED
+SEED_PATH = Config.SEED_PATH
+WORKERS = Config.WORKERS
+LOG_PATH = Config.LOG_PATH
+
+def run_page_handlers(page_id):
+    print(f"Crawling page: {page_id}")
+    try:
+        PageHandler(page_id)
+    except Exception as e:
+        print(f"Error at page: {page_id}")    
+        with open(f'{LOG_PATH}/page_{page_id}.log','w') as file:
+            file.write(str(e))
+    return
 
 
 crawler = Crawler(seed=SEED, seed_path=SEED_PATH)
-for i in range(10):
-    page_id = crawler.frontier.get_next_url()
-    print(f"Crawling page: {page_id}")
-    page_handler = PageHandler(page_id)
+print(f"Starting Crawler {'with' if SEED else 'without'} Seed\n ... executing {WORKERS} workers ...\n")
+
+with concurrent.futures.ThreadPoolExecutor(max_workers=WORKERS) as executor:
+    while True:
+        page_id = crawler.frontier.get_next_url()
+        if page_id is None:
+            sys.stdout.flush()
+            time.sleep(1)
+            continue
+        executor.submit(run_page_handlers(copy.deepcopy(page_id)))
--- app/config.py
+++ app/config.py
@@ -1,4 +1,8 @@
 class Config:
+    SEED = False
+    SEED_PATH = 'seed.txt'
+    WORKERS = 6
+    LOG_PATH = 'log'
     # WEB_DRIVER_LOCATION_CHROME = "../selenium/chromedriver"
     RENDERING_TIMEOUT = 5
     WEB_DRIVER_LOCATION_GECKO = "selenium/geckodriver"
