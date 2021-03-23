e49bd3500b530a4cbbcba9340c3666dd3449db2a
Luka Kavčič <luka.kavcic95@gmail.com>
2021-03-23 12:29:40 +0100

    disallow fix

--- app/frontier.py
+++ app/frontier.py
@@ -88,7 +88,7 @@ class Frontier:
         # handle if crawler is even allowed on url
         for disallow in robots_json['disallow']:
             # if crawler is not allowed on this url, update Page.accessed_time = -1 and call self function again 
-            if disallow == result_page.url[:len(disallow)]:
+            if disallow in result_page.url:
                 result_page.accessed_time = -1
                 result_page.page_type_code = 'DISALLOWED'
                 self.session.commit()
